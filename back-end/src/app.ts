96ffab591e11985d8a4c5be15e60f89e6f32cb7e
rahani <j.rahani@gmail.com>
2022-10-15 01:14:45 +0330

    add endpoint and error handlers

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,7 +1,9 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import compression from "compression";
 import bodyParser from "body-parser";
 import lusca from "lusca";
+import { topRatedValidator } from "./validators/github";
+import { topRatedController } from "./controllers/github/topRated";
 
 // Create Express server
 const app = express();
@@ -19,9 +21,23 @@ app.use(lusca.xssProtection(true));
 /**
  * Primary app routes.
  */
+app.post("/github/topRated", topRatedValidator, topRatedController);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+/**
+ * 404 error handlers
+ */
+app.use((req: Request, res: Response, next: NextFunction) => {
+  return res.status(404).json({ error: "Not Found" });
+});
+
+/**
+ * 500 error handlers
+ */
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  // console.log("Path: ", req.path);
+  // console.error("Error: ", err);
+
+  return res.status(500).json({ error: "500 Internal Server Error" });
 });
 
 export default app;
